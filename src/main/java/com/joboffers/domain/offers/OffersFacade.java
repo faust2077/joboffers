caab8c5d3c9754cca5baee4ed5554ef68851df5e
faust2077 <mrglassman2077@gmail.com>
2024-02-20 19:43:08 +0100

    Detached different branches development (from current branch).

--- src/main/java/com/joboffers/domain/offers/OffersFacade.java
+++ src/main/java/com/joboffers/domain/offers/OffersFacade.java
@@ -1,16 +1,4 @@
 package com.joboffers.domain.offers;
 
 public class OffersFacade {
-    public void findAllOffers() {
-
-    }
-
-    public void findOffersById(int id) {
-
-    }
-
-    public void saveOffer() {
-
-    }
-
 }
