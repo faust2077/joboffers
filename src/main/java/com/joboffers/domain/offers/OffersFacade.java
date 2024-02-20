299ac3328a3ebe14354683a9aec1ec603984b50b
faust2077 <mrglassman2077@gmail.com>
2024-02-20 19:11:58 +0100

    Created offers methods.

--- src/main/java/com/joboffers/domain/offers/OffersFacade.java
+++ src/main/java/com/joboffers/domain/offers/OffersFacade.java
@@ -1,4 +1,16 @@
 package com.joboffers.domain.offers;
 
 public class OffersFacade {
+    public void findAllOffers() {
+
+    }
+
+    public void findOffersById(int id) {
+
+    }
+
+    public void saveOffer() {
+
+    }
+
 }
